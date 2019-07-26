4b6cfea3e525684b1b34f124f86b73571742e92f
Robert Hönig <robhoenig@gmail.com>
2019-07-26 12:04:47 -0400

    Add (failing) test for Normal distribution.

---
+++ test/distributions.jl
@@ -0,0 +1,38 @@
+using Random
+
+function f(a,b)
+    rng = MersenneTwister(1234)
+    dist = Normal(a,b)
+    return sum(rand(rng, dist, 4))
+end
+@test gradient(f, 1, 2) = 1234
+
+# Yet to implement:
+#     uni_dists = [
+#         Arcsine(2, 4),
+#         Beta(2,2),
+#         BetaPrime(),
+#         Biweight(),
+#         Cauchy(),
+#         Chi(3),
+#         Chisq(2),
+#         Cosine(),
+#         Epanechnikov(),
+#         Erlang(),
+#         Exponential(),
+#         FDist(1, 1),
+#         Frechet(),
+#         Gamma(),
+#         InverseGamma(),
+#         InverseGaussian(),
+#         # Kolmogorov(),
+#         Laplace(),
+#         Levy(),
+#         Logistic(),
+#         LogNormal(1.0, 2.5),
+#         Normal(0.1, 2.5),
+#         Pareto(),
+#         Rayleigh(1.0),
+#         TDist(2),
+#         TruncatedNormal(0, 1, -Inf, 2),
+#     ]
